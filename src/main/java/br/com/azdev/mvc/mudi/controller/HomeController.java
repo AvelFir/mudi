9bbdadabb8c1293f380f2170af41e8e2711992cf
AvelFir <guichasemartinsgmr@hotmail.com>
2021-01-25 23:29:06 -0300

    transform Pedido in Entity and create PedidoRepository

--- src/main/java/br/com/azdev/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/azdev/mvc/mudi/controller/HomeController.java
@@ -1,27 +1,22 @@
 package br.com.azdev.mvc.mudi.controller;
 
 import br.com.azdev.mvc.mudi.model.Pedido;
+import br.com.azdev.mvc.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.math.BigDecimal;
-import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeController {
-    @GetMapping("/home")
+    @Autowired
+    private PedidoRepository pedidoRepository;
 
+    @GetMapping("/home")
     public String home(Model model){
-        Pedido pedido = new Pedido();
-        pedido.setNome("Echo Dot");
-        pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/61a%2BK6x-RML._AC_SL1000_.jpg");
-        pedido.setUrlProduto("https://www.amazon.com.br/gp/product/B084DWCZY6/?pf_rd_r=EDX5VZFY7GPF50FQRHDH&pf_rd_p=5d8f5ea6-a0a3-4bb0-aa59-ddde156f6b37");
-        pedido.setDescricao("Descrição Qualquer");
-        List<Pedido> pedidos = Arrays.asList(pedido);
+        List<Pedido> pedidos = pedidoRepository.findAll();
         model.addAttribute("pedidos", pedidos);
         return "home";
     }
--- src/main/java/br/com/azdev/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/azdev/mvc/mudi/model/Pedido.java
@@ -1,9 +1,17 @@
 package br.com.azdev.mvc.mudi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
 public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
     private BigDecimal valor;
     private LocalDate dataEntrega;
