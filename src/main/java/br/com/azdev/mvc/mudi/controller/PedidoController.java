00212ea1e92f9036f0186deac46f76bd983b2fc1
AvelFir <guichasemartinsgmr@hotmail.com>
2021-01-26 06:32:23 -0300

    added search by status

--- src/main/java/br/com/azdev/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/azdev/mvc/mudi/controller/PedidoController.java
@@ -6,12 +6,14 @@ import br.com.azdev.mvc.mudi.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("pedido")
